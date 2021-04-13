ae75328a818e2fae2e86f6cf538a20b6787bee8d
Muhammad Dio Syahrizal <diosyahrizal@gmail.com>
2021-04-13 11:33:17 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 module.exports = {
-  assetPrefix: ghPages ? "/diosyahrizal.github.io/blog-nextjs/" : "", // customize this value
+  assetPrefix: "/diosyahrizal.github.io/", // customize this value
 };
